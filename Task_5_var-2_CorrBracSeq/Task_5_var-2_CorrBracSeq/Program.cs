645a0968c50bfe4965ad9f4343bb9ef1d97eacae
SergSmith64 <sergsmith2008@yandex.ru>
2019-04-30 01:55:00 +0400

    commit_02_Task_Complete

---
+++ Task_5_var-2_CorrBracSeq/Task_5_var-2_CorrBracSeq/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5_var_2_CorrBracSeq
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Правильная скобочная последовательность это (){}[] ");
+            Console.WriteLine("или ({})[{}] или даже [{}({})]");
+            Console.WriteLine("ВВЕДИТЕ НАБОР СКОБОК ДЛЯ ПРОВЕРКИ :");
+            var s = Console.ReadLine();
+
+            // Console.WriteLine("Вы ввели : " + s);
+
+            var checker = new BracketsChecker();
+
+            foreach (var ch in s)
+                checker.Put(ch);
+
+            if (checker.Balanced == true)
+            {
+                Console.WriteLine("Вы все слелали ПРАВИЛЬНО!");
+            }
+            else Console.WriteLine("Вы допустили ошибку!");
+
+            Console.ReadKey();
+        }
+
+        class BracketsChecker
+        {
+            private readonly string _opening = "([{";
+            private readonly string _closing = ")]}";
+
+            private bool _cantBeBalanced;
+
+            private Stack<int> _opened = new Stack<int>();
+
+            public bool Balanced => !_cantBeBalanced && !_opened.Any();
+
+            public void Put(char ch)
+            {
+                if (_cantBeBalanced) return;
+
+                int index = _opening.IndexOf(ch);
+
+                if (index != -1)
+                {
+                    _opened.Push(index);
+                    return;
+                }
+
+                index = _closing.IndexOf(ch);
+
+                if (index != -1)
+                {
+                    if (!_opened.Any() || _opened.Peek() != index)
+                    {
+                        _cantBeBalanced = true;
+                        _opened.Clear();
+                        _opened.TrimExcess();
+                        return;
+                    }
+
+                    _opened.Pop();
+                    return;
+                }
+            }
+
+        }
+    }
+}
+
